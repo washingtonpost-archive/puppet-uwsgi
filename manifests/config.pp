f4d95c729637a68be62a3139308257642ee4958d
Jason Bartz <jrbartz@gmail.com>
2013-01-17 16:06:28 -0500

    Making require param more specific

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,12 +3,12 @@ class uwsgi::config {
     file {'/etc/default/uwsgi':
         ensure => present,
         content => template('uwsgi/default.erb'),
-        require => Package['uwsgi'],
+        require => Package['uwsgi::package'],
     }
 
     file {'/etc/uwsgi/default.ini':
         ensure => present,
         content => template('uwsgi/default.ini.erb'),
-        require => Package['uwsgi'],
+        require => Package['uwsgi::package'],
     }
 }
