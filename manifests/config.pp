0ccb19043b5562ca2785e0a580dec3f85ff232af
Jason Bartz <jrbartz@gmail.com>
2013-01-17 16:11:47 -0500

    fixing class references

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,12 +3,12 @@ class uwsgi::config {
     file {'/etc/default/uwsgi':
         ensure => present,
         content => template('uwsgi/default.erb'),
-        require => Package['uwsgi::package'],
+        require => Class['uwsgi::package'],
     }
 
     file {'/etc/uwsgi/default.ini':
         ensure => present,
         content => template('uwsgi/default.ini.erb'),
-        require => Package['uwsgi::package'],
+        require => Class['uwsgi::package'],
     }
 }
